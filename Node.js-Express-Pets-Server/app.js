7a4e0dd4d5dcee553c1dc46eac5f563977117801
patricksequeiravega <patricksvega913@gmail.com>
2021-07-02 00:17:07 -0600

    first commit

---
+++ Node.js-Express-Pets-Server/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const home = require('./routes/home.js');
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.use('/home', home);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
+
